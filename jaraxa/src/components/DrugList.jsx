033cfa2996819698c74a3a93f1a33b66ea572174
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-06-17 19:21:41 +0200

    more materialui testing + api investigation

--- jaraxa/src/components/DrugList.jsx
+++ jaraxa/src/components/DrugList.jsx
@@ -1,20 +1,98 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const drugs = [
-  { id: 1, name: 'Drug 1', description: 'Description of Drug 1' },
-  { id: 2, name: 'Drug 2', description: 'Description of Drug 2' },
-  { id: 3, name: 'Drug 3', description: 'Description of Drug 3' },
-];
+import successResponse from "../data/successResponse.json";
+// import errorResponse from "../data/errorResponse.json";
 
-export const DrugList = () => (
-  <div>
-    <h1>Drugs</h1>
-    <ul>
-      {drugs.map(drug => (
-        <li key={drug.id}>
-          <Link to={`/drug/${drug.id}`}>{drug.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+import {
+  Box,
+  TextField,
+  Typography,
+  Button,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import { useState } from "react";
+
+export const DrugList = () => {
+  const [checked, setChecked] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submited");
+  };
+  const handleCheck = (event) => {
+    setChecked(event.target.checked);
+  };
+  return (
+    <div>
+      <Typography
+        sx={{ display: "flex", justifyContent: "center" }}
+        variant="h1"
+        color="initial"
+      >
+        Drugs
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Box
+          sx={{
+            width: 1,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: { xs: "column", sm: "row" },
+            justifyContent: "center",
+            mt: 4,
+            gap: 2,
+          }}
+        >
+          {/* substance_name */}
+          <TextField
+            label="Active ingredients"
+            type="search"
+            variant="outlined"
+            margin="normal"
+            sx={{ my: 0 }}
+          />
+          {/* generic_name */}
+          <TextField
+            label="Generic Name"
+            type="search"
+            variant="outlined"
+            margin="normal"
+            sx={{ my: 0 }}
+          />
+          {/* manufacturer_name */}
+          <TextField
+            label="Manufacturer"
+            type="search"
+            variant="outlined"
+            margin="normal"
+            sx={{ my: 0 }}
+          />
+
+          {/* openfda.product_type (HUMAN OTC DRUG) */}
+          <FormControlLabel
+            label="OTC"
+            control={
+              <Checkbox
+                checked={checked}
+                onChange={handleCheck}
+                color="primary"
+              />
+            }
+          />
+
+          <Button variant="outlined" color="primary" type="submit">
+            Search
+          </Button>
+        </Box>
+      </form>
+      <ul>
+        {successResponse.results.map((drug) => (
+          <li key={drug.set_id}>
+            <Link to={`/drug/${drug.set_id}`}>{drug.openfda.brand_name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
